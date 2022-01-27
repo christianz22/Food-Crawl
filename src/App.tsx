7aefa0eee0b4e0406f01e97bf4d4701416bce158
christian zimmerman <czimmerman202@gmail.com>
2022-01-26 16:22:57 -0800

    app changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 
 import './App.css';
-import { useState } from 'react';
+// import { useState } from 'react';
 import Reviews from './Rewiews';
 import RestaurantInfo from './RestaurantInfo';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Restaurant } from './RestaurantInterface';
+// import { Restaurant } from './RestaurantInterface';
 import LandingPage from './LandingPage';
 import BucketList from './components/BucketList/BucketList'
 import foodAPI from './RestuarantAPI';
