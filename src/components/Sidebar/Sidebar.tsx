56be63a17b4e96dae91d284715ac69f80b1df507
Jarod <jdjohnek@gmail.com>
2022-01-26 21:15:51 -0500

    minor fix

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,4 @@
-import React from 'react'
-import * as FaIcons from "react-icons/fa"
 import * as AiIcons from "react-icons/ai"
-import * as GiIcons from 'react-icons/gi'
 import { BsBucketFill } from "react-icons/bs";
 
 export const SidebarData = [
